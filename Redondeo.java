49c9afd53b4326de49eecc7446ec632e1ba74960
Ale <alepenalvaherrero@gmail.com>
2024-11-23 11:00:32 +0100

    Nuevos ejercicios

---
+++ Redondeo.java
@@ -0,0 +1,18 @@
+package com.ejercicios;
+
+public class Redondeo {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double numero=16.35;
+		
+		System.out.println("El redondeo de " + numero + " es " + Math.round(numero));
+	}
+
+}
+
+
+
---
+++ Refundición.java
@@ -0,0 +1,20 @@
+package com.ejercicios;
+
+public class Refundición {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+			double num1=5.85;
+			
+			int resultado=(int)Math.round(num1); //refundición
+					
+			System.out.println(resultado);
+			
+
+
+	}
+
+}
