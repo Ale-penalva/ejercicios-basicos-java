6e654711b74ab742cd8d3c541dafb39272ccbee1
Ale <alepenalvaherrero@gmail.com>
2024-11-23 10:51:04 +0100

    Segundo ejercicio, raices cuadradas

---
+++ RaícesCuadradas.java
@@ -0,0 +1,15 @@
+package com.ejercicios;
+
+public class RaícesCuadradas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Calcular la raíz cuadrada de un número utilizando Math.sqrt()
+		double raiz=Math.sqrt(36);
+		
+		System.out.println(raiz);
+		
+	}
+
+}
